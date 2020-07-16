88d9719ff0ecce7435dee4829c7b9c0aa6a61aeb
s-ayush2903 <ayush_1901cb14@iitp.ac.in>
2020-07-17 01:48:15 +0530

    Initialized Retrofit Stuff

---
+++ app/src/main/java/com/stvayush/recipebook/requests/RecipeApi.kt
@@ -0,0 +1,8 @@
+package com.stvayush.recipebook.requests
+
+/**
+ * Authored by s-ayush2903 on 17/7/20
+ */
+
+interface RecipeApi {
+}
---
+++ app/src/main/java/com/stvayush/recipebook/requests/ServiceGenerator.kt
@@ -0,0 +1,20 @@
+package com.stvayush.recipebook.requests
+
+import com.stvayush.recipebook.utils.Constants
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+/**
+ * Authored by s-ayush2903 on 17/7/20
+ */
+
+object ServiceGenerator {
+
+    var retrofitBuilder = Retrofit.Builder()
+        .baseUrl(Constants.BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+    private var retrofit = retrofitBuilder.build()
+
+    private var recipeApi = retrofit.create(RecipeApi::class.java)
+    public var getRecipeApi = recipeApi
+}

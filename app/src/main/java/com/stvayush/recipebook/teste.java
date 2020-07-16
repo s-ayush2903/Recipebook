e8422773e0ea70163b5208c885bf79bbf8d82a38
s-ayush2903 <ayush_1901cb14@iitp.ac.in>
2020-07-17 00:32:13 +0530

    Initial Fundamental Settings

---
+++ app/src/main/java/com/stvayush/recipebook/teste.java
@@ -0,0 +1,22 @@
+package com.stvayush.recipebook;
+
+import android.view.View;
+import android.widget.ProgressBar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * Authored by s-ayush2903 on 16/7/20
+ */
+
+class teste  {
+//for testing purposes
+    private ProgressBar progressBar;
+
+    private void showProgressBar(boolean visible) {
+        progressBar.setVisibility(visible ? View.VISIBLE : View.INVISIBLE);
+    }
+
+
+
+}

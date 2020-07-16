806f8d21d64324da9bc3f7bbb414d332214e5128
Restyled.io <commits@restyled.io>
2020-07-16 19:30:56 +0000

    Restyled by clang-format

--- app/src/main/java/com/stvayush/recipebook/teste.java
+++ app/src/main/java/com/stvayush/recipebook/teste.java
@@ -2,22 +2,17 @@ package com.stvayush.recipebook;
 
 import android.view.View;
 import android.widget.ProgressBar;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 /**
  * Authored by s-ayush2903 on 16/7/20
  */
 
-class teste  {
-//for testing purposes
-    private ProgressBar progressBar;
+class teste {
+  // for testing purposes
+  private ProgressBar progressBar;
 
-    private void showProgressBar(boolean visible) {
-        progressBar.setVisibility(visible ? View.VISIBLE : View.INVISIBLE);
-    }
-
-
-
-
+  private void showProgressBar(boolean visible) {
+    progressBar.setVisibility(visible ? View.VISIBLE : View.INVISIBLE);
+  }
 }

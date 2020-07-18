a87792577d21c20d79f8b8585cf6b5ea07ff4be4
s-ayush2903 <ayush_1901cb14@iitp.ac.in>
2020-07-19 00:04:58 +0530

    Subtle Fixes

--- app/src/main/java/com/stvayush/recipebook/datamodelling/Recipe.kt
+++ app/src/main/java/com/stvayush/recipebook/datamodelling/Recipe.kt
@@ -12,64 +12,8 @@ import kotlinx.android.parcel.Parcelize
 data class Recipe(
     private val title: String?,
     private val publisher: String?,
-    private val ingredients: Array<String>?,
-    private val recipeId: String?,
-    private val socialRank: Float
+    private val recipe_id: String?,
+    private val social_rank: Float?
 ) : Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readString(),
-        parcel.readString(),
-        parcel.createStringArray(),
-        parcel.readString(),
-        parcel.readFloat()
-    ) {
-    }
-
-
-
-    companion object : Parceler<Recipe> {
-
-        override fun Recipe.write(parcel: Parcel, flags: Int) {
-            parcel.writeString(title)
-            parcel.writeString(publisher)
-            parcel.writeStringArray(ingredients)
-            parcel.writeString(recipeId)
-            parcel.writeFloat(socialRank)
-        }
-
-        override fun create(parcel: Parcel): Recipe {
-            return Recipe(parcel)
-        }
-    }
-
-    override fun toString(): String {
-        return "Recipe(title=$title, publisher=$publisher, ingredients=${ingredients?.contentToString()}, recipeId=$recipeId, socialRank=$socialRank)"
-    }
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Recipe
-
-        if (title != other.title) return false
-        if (publisher != other.publisher) return false
-        if (ingredients != null) {
-            if (other.ingredients == null) return false
-            if (!ingredients.contentEquals(other.ingredients)) return false
-        } else if (other.ingredients != null) return false
-        if (recipeId != other.recipeId) return false
-        if (socialRank != other.socialRank) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = title?.hashCode() ?: 0
-        result = 31 * result + (publisher?.hashCode() ?: 0)
-        result = 31 * result + (ingredients?.contentHashCode() ?: 0)
-        result = 31 * result + (recipeId?.hashCode() ?: 0)
-        result = 31 * result + socialRank.hashCode()
-        return result
-    }
 }
+

d97b2c46d0b6c9f9f554e983dc797fd4b6f61cfb
adrishyantee <adrishyantee@gmail.com>
2020-12-06 18:51:04 +0530

    Set the button vis to gonr and set up the recyclerView with placeholder

--- app/src/main/java/com/stvayush/recipebook/core/BaseActivity.kt
+++ app/src/main/java/com/stvayush/recipebook/core/BaseActivity.kt
@@ -5,11 +5,15 @@ import android.widget.FrameLayout
 import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.stvayush.recipebook.R
-
+import kotlinx.android.synthetic.main.activity_base.*
 /**
  * Authored by s-ayush2903 on 16/7/20
  */
+//added to create the recyclerView
+private lateinit var LinearLayoutManager: LinearLayoutManager
+
 abstract class BaseActivity : AppCompatActivity() {
     var progressBar: ProgressBar? = null
     override fun setContentView(layoutResID: Int) {
@@ -20,6 +24,9 @@ abstract class BaseActivity : AppCompatActivity() {
         progressBar = constraintLayout.findViewById(R.id.progress_bar)
         layoutInflater.inflate(layoutResID, frameLayout, true)
         super.setContentView(constraintLayout)
+
+        LinearLayoutManager = LinearLayoutManager(this)  //added for creating RecyclerView
+        recyclerView.layoutManager = LinearLayoutManager
     }
 
     fun showProgressBar(visible: Boolean) {
